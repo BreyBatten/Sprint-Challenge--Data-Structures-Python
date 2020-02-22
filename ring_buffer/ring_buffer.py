a7b0e18ad841f008e610286210529a0e9b57620c
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2020-02-22 18:48:31 -0500

    ring_buffer append method works, fixing get method

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,26 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # if there's room in the buffer, add new item to tail
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
+        # if there's not room, remove the head and add the tail
+        elif self.storage.length == self.capacity:
+            self.storage.remove_from_head()
+            self.storage.add_to_tail(item)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        if self.storage.head:
+            list_buffer_contents.append(self.storage.head.value)
+            current_node = self.storage.head
+
+            for i in range(self.storage.length - 1):
+                list_buffer_contents.append(current_node.next.value)
+                current_node = current_node.next
 
         return list_buffer_contents
 
